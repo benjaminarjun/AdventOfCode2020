57cc7ec416c4cc8f3a6777c7e274cb30a2045772
Ben <benarthur91@gmail.com>
2020-12-05 07:30:23 -0800

    Add AOC util test to check parsing as string

--- solutions/tests/test_aoc_util.py
+++ solutions/tests/test_aoc_util.py
@@ -7,5 +7,8 @@ class TestAocUtil(unittest.TestCase):
         document = """1
 2
 3"""
-        contents = parse_file_contents(document, entry_trans=int)
-        self.assertEqual([1, 2, 3], contents)
+        int_contents = parse_file_contents(document, entry_trans=int)
+        self.assertEqual([1, 2, 3], int_contents)
+
+        str_contents = parse_file_contents(document, entry_trans=str)
+        self.assertEqual(['1', '2', '3'], str_contents)
