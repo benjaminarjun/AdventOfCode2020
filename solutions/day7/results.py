8a05985b6be080e22d09ce3775086f46f3f2d365
Ben <benarthur91@gmail.com>
2020-12-07 22:17:51 -0800

    Day 7 part 1 complete

---
+++ solutions/day7/results.py
@@ -0,0 +1,51 @@
+import re
+from ..aoc_util import get_data
+
+
+def parse_bag_container_requirements(req_lines):
+    reqs = {}
+
+    line_ptn = re.compile('(.*) bags contain (.*)\.')
+    bag_list_ptn = re.compile('((\d+) (.*)|no other) bags?')
+
+    for line in req_lines:
+        line_match = re.match(line_ptn, line)
+        contained_bags_str = line_match.group(2)
+
+        if line_match.group(2) == 'no other bags':
+            reqs[line_match.group(1)] = []
+            continue
+
+        contained_bags = []
+        for bag_str in contained_bags_str.split(', '):
+            bag_match = re.match(bag_list_ptn, bag_str)
+
+            contained_bags.append((int(bag_match.group(2)), bag_match.group(3)))
+
+        reqs[line_match.group(1)] = contained_bags
+
+    return reqs
+
+
+def get_valid_supercontainers(color, bag_container_reqs):
+    traversed_nodes = []
+    req_colors = {k: [z[1] for z in v] for k, v in bag_container_reqs.items()}
+    these_containers = [k for k, v in req_colors.items() if color in v]
+
+    while len(these_containers) > 0:
+        traversed_nodes.extend(these_containers)
+        these_containers = [
+            k for k, v in req_colors.items() \
+            if any([z for z in these_containers if z in v])
+            and k not in traversed_nodes
+        ]
+
+    return set(traversed_nodes)
+
+
+if __name__ == '__main__':
+    data = get_data(7, entry_trans=str)
+    bag_container_reqs = parse_bag_container_requirements(data)
+    print(f'Part 1:  {len(get_valid_supercontainers("shiny gold", bag_container_reqs))}')
+
+    print(f'Part 2:  NOT IMPLEMENTED')
---
+++ solutions/day7/test_lib.py
@@ -0,0 +1,40 @@
+import unittest
+from .results import parse_bag_container_requirements, get_valid_supercontainers
+
+
+class TestBagColorRules(unittest.TestCase):
+    @property
+    def data(self):
+        return [
+            'light red bags contain 1 bright white bag, 2 muted yellow bags.',
+            'dark orange bags contain 3 bright white bags, 4 muted yellow bags.',
+            'bright white bags contain 1 shiny gold bag.',
+            'muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.',
+            'shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.',
+            'dark olive bags contain 3 faded blue bags, 4 dotted black bags.',
+            'vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.',
+            'faded blue bags contain no other bags.',
+            'dotted black bags contain no other bags.',
+        ]
+
+    def test_parse_bag_container_requirements(self):
+        expected = {
+            'light red': [(1, 'bright white'), (2, 'muted yellow')],
+            'dark orange': [(3, 'bright white'), (4, 'muted yellow')],
+            'bright white': [(1, 'shiny gold')],
+            'muted yellow': [(2, 'shiny gold'), (9, 'faded blue')],
+            'shiny gold': [(1, 'dark olive'), (2, 'vibrant plum')],
+            'dark olive': [(3, 'faded blue'), (4, 'dotted black')],
+            'vibrant plum': [(5, 'faded blue'), (6, 'dotted black')],
+            'faded blue': [],
+            'dotted black': [],
+        }
+
+        self.assertEqual(expected, parse_bag_container_requirements(self.data))
+
+    def test_get_valid_supercontainers(self):
+        bag_container_reqs = parse_bag_container_requirements(self.data)
+        expected = set(['bright white', 'muted yellow', 'dark orange', 'light red'])
+        actual = get_valid_supercontainers('shiny gold', bag_container_reqs)
+
+        self.assertEqual(expected, actual)
